6f9bb4195c1a5f44c7b56fb74c4ceffce943bead
ShrutiMore24 <97089262+ShrutiMore24@users.noreply.github.com>
2022-02-26 17:07:32 +0530

    Add files via upload

---
+++ draw-pattern.java
@@ -0,0 +1,17 @@
+ class RightTrianglePattern   
+{   
+public static void main(String args[])   
+{   
+
+int i, j, row=6;   
+for(i=0; i<row; i++)   
+{   
+for(j=0; j<=i; j++)   
+{      
+System.out.print("* ");   
+}   
+//printing each linee aft  
+System.out.println();   
+}   
+}   
+}  
